21021f683faedbe9362d5f57febebd2f8857c8c1
Akaishi Toshiya <Toshiya14@live.jp>
2022-10-08 16:01:38 +0800

    dev

---
+++ BMFSharpRenderer/Proxies/IRenderingProxy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMEGo.Game.BMFSharp.Proxies;
+
+public interface IRenderingProxy
+{
+}
